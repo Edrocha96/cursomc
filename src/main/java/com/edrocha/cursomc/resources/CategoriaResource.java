d039d7a41f390062c75cfb90ebc70dfd4e833018
Edgar Rocha <ehenrique.rocha@hotmail.com>
2021-10-24 13:07:54 -0300

    Criando Repository e Service para Categoria

--- src/main/java/com/edrocha/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/edrocha/cursomc/resources/CategoriaResource.java
@@ -1,29 +1,26 @@
 package com.edrocha.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edrocha.cursomc.domain.Categoria;
+import com.edrocha.cursomc.services.CategoriaService;
 
 @RestController
-@RequestMapping(value ="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria ca1 = new Categoria(1,"Informatica");
-		Categoria ca2 = new Categoria(1,"Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(ca1);
-		lista.add(ca2);
-		
-		
-		return lista;
+
+	@Autowired
+	private CategoriaService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
