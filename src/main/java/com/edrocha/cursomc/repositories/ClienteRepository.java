5e9c494d35bfc818c500f81e25aee0e7bf71604d
Edgar Rocha <ehenrique.rocha@hotmail.com>
2022-03-20 13:02:53 -0300

    Cliente TipoCliente, telefones e enderecos

---
+++ src/main/java/com/edrocha/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.edrocha.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edrocha.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
