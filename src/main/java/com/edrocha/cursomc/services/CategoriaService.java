6f3cdba01c78454df6e3cac9cd1f9fe4b68ba408
Edgar Rocha <ehenrique.rocha@hotmail.com>
2022-05-01 17:10:45 -0300

    validacao sintatica com Bean Validation

--- src/main/java/com/edrocha/cursomc/services/CategoriaService.java
+++ src/main/java/com/edrocha/cursomc/services/CategoriaService.java
@@ -12,6 +12,7 @@ import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Service;
 
 import com.edrocha.cursomc.domain.Categoria;
+import com.edrocha.cursomc.dto.CategoriaDTO;
 import com.edrocha.cursomc.repositories.CategoriaRepository;
 import com.edrocha.cursomc.services.exceptions.DataIntegrityException;
 import com.edrocha.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+		
 	}
 }
